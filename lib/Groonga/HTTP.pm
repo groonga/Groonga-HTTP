de94238301cffd05e6f5fb7ed659318e5ab9728d
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-12-15 16:15:57 +0900

    Add a support for Groonga's command "status"

--- lib/Groonga/HTTP.pm
+++ lib/Groonga/HTTP.pm
@@ -1,5 +1,8 @@
 package Groonga::HTTP;
 
+use LWP;
+use JSON 'decode_json';
+
 use strict;
 use warnings;
 
@@ -17,6 +20,40 @@ sub new {
         $port = $self->{port};
     }
     return bless $self, $class;
+}
+
+sub status {
+    my $query = _make_query({command => 'status'});
+    my $http_response = _send_to_query($query);
+
+    if ($http_response->code == 200) {
+        my $command_response_raw = $http_response->content;
+        my $command_response = decode_json($command_response_raw);
+        my $groonga_reposnse_code = _get_groonga_response_code($command_response);
+        if ($groonga_reposnse_code == 0) {
+            return $command_response;
+        } else {
+            return $groonga_reposnse_code;
+        }
+    } else {
+        return $response->code;
+    }
+}
+
+sub _make_query {
+    my %args = @_;
+    my $command = '';
+
+    switch ($args{'command'}) {
+        case 'status' { $command = 'status' }
+    }
+    return "http://${host}:${port}/d/${command}";
+}
+
+sub _send_to_query {
+    my $query = shift;
+    my $user_agent = LWP::UserAgent->new;
+    return $user_agent->get($query);
 }
 
 1;
