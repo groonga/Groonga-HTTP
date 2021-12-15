fedac55d5b9962dd5ff0f09560378fc9d594c065
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-12-15 14:46:14 +0900

    Add support for arguments

--- lib/Groonga/HTTP.pm
+++ lib/Groonga/HTTP.pm
@@ -5,5 +5,12 @@ use warnings;
 sub new {
     my ($class, %args) = @_;
     my $self = {%args};
+
+    if ($self->{host}) {
+        $host = $self->{host};
+    }
+    if ($self->{port}) {
+        $port = $self->{port};
+    }
     return bless $self, $class;
 }
