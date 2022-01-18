2de10ad51971936ee3caf4b18ca6a4ca70b29a0d
Horimoto Yasuhiro <horimoto@clear-code.com>
2022-01-18 16:26:11 +0900

    Add a new step for testing installation of Groonga-HTTP module

---
+++ t/install-test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) 2021-2022  Horimoto Yasuhiro <horimoto@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -exu
+
+os=$(cut -d: -f4 /etc/system-release-cpe)
+major_version=$(cut -d: -f5 /etc/system-release-cpe | grep -o "^[0-9]")
+
+case ${major_version} in
+  7)
+    DNF=yum
+    ;;
+  *)
+    DNF="dnf --enablerepo=powertools"
+    ;;
+esac
+
+${DNF} install -y \
+  gcc \
+  perl \
+  perl-App-cpanminus
+
+# Install Perl Modules
+cpanm --notest git://github.com/groonga/Groonga-HTTP.git@0.01
+
+perl -e "use Groonga::HTTP"
