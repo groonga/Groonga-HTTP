8b2e6e6bb668e3abc23aff0ffde783fdfb449647
Horimoto Yasuhiro <horimoto@clear-code.com>
2022-01-27 16:40:50 +0900

    Use the latest version

--- t/install-test.sh
+++ t/install-test.sh
@@ -36,6 +36,6 @@ ${DNF} install -y \
   perl-App-cpanminus
 
 # Install Perl Modules
-cpanm --notest git://github.com/groonga/Groonga-HTTP.git@0.02
+cpanm --notest git://github.com/groonga/Groonga-HTTP.git@0.03
 
 perl -e "use Groonga::HTTP"
